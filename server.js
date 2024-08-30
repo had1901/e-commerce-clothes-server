7d9141a2ca440328f576ee72aa41aeee5a679401
had191 <senna2k20@gmail.com>
2024-08-31 00:15:04 +0700

    Init project

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+
+const app = express()
+const PORT = process.env.POST || 5000
+
+
+app.use(cors({
+    origin: 'http://localhost:3000/',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Cache-Control',
+        'Expires',
+        'Pragma',
+    ],
+}))
+app.use(cookieParser())
+app.use(express.json())
+
+const connect = async () => {
+    await mongoose.connect('mongodb+srv://ductoclx01:1912001d@cluster0.vcyzw.mongodb.net/')
+        .then(() => console.log('Connect to mongooseDB'))
+        .catch(err => console.error(err))
+
+    app.listen(PORT, () => console.log('Connect to server'))
+}
+connect()
+
